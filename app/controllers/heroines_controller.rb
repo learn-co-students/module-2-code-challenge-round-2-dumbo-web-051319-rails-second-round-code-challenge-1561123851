b49682cdba58b08e4d8d142f22d5e5cbeed19974
github username <github email address>
2019-06-21 10:17:30 -0400

    finished deliverables, joshua suskin

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,30 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    redirect_to @heroine
+    # @heroine = Heroine.new(heroine_params)
+    # if @heroine.valid?
+    #   @heroine.save
+    #   redirect_to @heroine
+    # else
+    #   flash[:errors] = "Name must be unique"
+    # end
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
 end
