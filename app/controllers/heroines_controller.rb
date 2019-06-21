7640a2a9e3960b5fcf97682625efa3d9a7d2dfbe
Stefanos <stefanosu422@gmail.com>
2019-06-21 11:12:20 -0400

    Done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,27 @@
 class HeroinesController < ApplicationController
   def index
-    @heroines = Heroine.all
+    @heroines = Heroine.search(params[:search])
   end
+
+def show 
+  @herione = Heroine.find(params[:id])
+end
+
+def new 
+  @heroine = Heroine.new 
+end
+
+def create
+  heroine = Heroine.create(heroine_params) 
+  redirect_to heroine
+end
+
+
+private 
+
+def heroine_params
+  params.require(:heroine).permit(:power_id, :name, :super_name, :search)
+  
+end
+
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,27 @@
 class Heroine < ApplicationRecord
-end
+    belongs_to :power
+
+    validates :super_name, uniqueness: true
+
+
+    def self.search(search) 
+        if search 
+            heroine_power = heroine_power.find_by([name: search]) 
+            if heroine_power
+                self.where( power_id: heroine_power ) 
+            else  
+                Heroine.all  
+            end 
+        else  
+            Heroine.all 
+        end 
+    end 
+  end
+
+
+
+
+ 
+
+# 7. Make sure no two heroines have the same super name. -->
+
