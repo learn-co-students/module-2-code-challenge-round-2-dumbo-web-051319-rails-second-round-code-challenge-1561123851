8ff57e2ee25926cf3a11ccd7ae8f57766796cd7c
Toni Lamont <tlam212@gmail.com>
2019-06-21 11:12:18 -0400

    finished

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,29 @@
 class HeroinesController < ApplicationController
+
   def index
     @heroines = Heroine.all
+  end
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+  def new
+    @heroine = Heroine.new
+  end
+  def create
+    @heroine = Heroine.create(heroine_params)
+    if @heroine.valid?
+    redirect_to @heroine
+    else
+    redirect_to new_heroine_path
+    end
+  end
+
+
+
+
+
+  private
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
   end
 end
