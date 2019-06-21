42fd4fedbd91c96be8407c8d84fbd816dedaec9b
“hannah6530” <hannah6530@gmail.com>
2019-06-21 11:06:36 -0400

    hannah, done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,29 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+
+    @heroine = Heroine.create(heroine_params)
+    redirect_to @heroine
+
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+    @power = @heroine.power
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
+
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,26 @@
 class PowersController < ApplicationController
   def index
     @powers = Power.all
+    @heroines = Heroine.all
+    # @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @power = Power.new
+    @heroines = Heroine.all
+  end
+
+  def create
+    @power = Power.create(power_params)
+  end
+
+  def show
+    @power = Power.find(params[:id])
+  end
+
+  private
+
+  def power_params
+    params.require(:power).permit(:name, :description)
   end
 end
